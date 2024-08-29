d58288929fde67fe2266fc0501248901fa35202a
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-08-29 20:39:26 -0300

    test: creando código de ejemplo de comunicación Ardunino Nano - Python

---
+++ tests/serial/main.c
@@ -0,0 +1,21 @@
+#include "gpio.h"
+#include "serial.h"
+
+int main(void) {
+  int n = 2;
+  int button = 12;
+  int exit = 2;
+  gpio_input(button);
+  gpio_input(exit);
+  serial_init();
+
+  while (1) {
+    if (gpio_pin(button, GET) == ON) {
+      serial_put_char('1');
+    }
+
+    if (gpio_pin(exit, GET) == ON) {
+      serial_put_char('q');
+    }
+  }
+}
---
+++ tests/serial/main.py
@@ -0,0 +1,12 @@
+import serial
+
+baudrate = 9600
+nano = serial.Serial('/dev/ttyUSB0', baudrate, timeout=1)
+
+msg = 0;
+print("Comenzando")
+while msg != 'q':
+    msg = nano.read(1).decode('latin-1').strip();
+    print(msg)
+    if msg == '1':
+        print("Botón 1 presionado")
