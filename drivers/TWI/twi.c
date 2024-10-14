6c8dbd04bb80aa7f821ab185f16dd1006895887c
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-10-13 21:46:53 -0300

    feat: implementación inicial del driver para el módulo TWI

---
+++ drivers/TWI/twi.c
@@ -0,0 +1,182 @@
+/**********************************************************************
+ *
+ * TWI (I²C) Atmega328p Driver
+ *
+ **********************************************************************/
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// CONSTANTS
+#define F_CPU 16000000      // FRECUENCIA DEL PROCESADOR: 16 MHz
+#define F_I2C 100000        // FRECUENCIA DE COMUNICACIÓN: 100 kHz
+#define DEFAULT_SLA 0x10    // DIRECCIÓN DEL ESCLAVO POR DEFECTO
+
+// PRESCALER VALUES
+#define PRESCALER_1  ((0 << TWPS1) | (0 << TWPS0))
+#define PRESCALER_4  ((0 << TWPS1) | (1 << TWPS0))
+#define PRESCALER_16 ((1 << TWPS1) | (0 << TWPS0))
+#define PRESCALER_64 ((1 << TWPS1) | (1 << TWPS0))
+
+// SETTINGS
+#define SLA_W(address)	(address<<1)
+#define SLA_R(address)	((address<<1) | (1<<0))
+#define ENABLE_TWI  (1 << TWEN)
+#define ENABLE_ACK  (1 << TWEA)
+#define GEN_START   (1 << TWSTA)
+#define GEN_STOP    (1 << TWSTO)
+#define CLEAR_INT   (1 << TWINT)
+
+// MISC
+#define STATUS_REG_MASK 0xF8
+
+/**********************************************************************
+ * STATUS CODES
+ **********************************************************************/
+
+// Status Codes from datasheet
+#define START_CONDITION_TRANSMITTED 0x08
+#define SLA_W_TRANSMITTED_ACK_RECEIVED 0x18
+#define SLA_W_TRANSMITTED_NO_ACK_RECEIVED 0x20
+#define ARBITRATION_LOST 0x38
+#define SLA_R_TRANSMITTED_ACK_RECEIVED 0x40
+#define SLA_R_TRANSMITTED_NO_ACK_RECEIVED 0x48
+#define SLA_R_RECEIVED_ACK_TRANSMITTED 0XA8
+#define I2C_TRANSMISSION_COMPLETE (twi->twcr & (1 << TWINT))
+
+// Return Status Code
+enum twi_status {
+    SUCCESS = 0,
+    ERR_START_FAILED = 1,
+    ERR_ARBITRATION_LOST = 2,
+    ERR_NO_ACK = 3,
+    ERR_MEMORY_ALLOCATION_FAILED = 4,
+};
+
+/**********************************************************************
+ * TWI STRUCTURE
+ **********************************************************************/
+
+typedef struct {
+    uint8_t twbr;       // TWI Bit Rate Register
+    uint8_t twsr;       // TWI Status Register
+    uint8_t twar;       // TWI (Slave) Address Register
+    uint8_t twdr;       // TWI Data Register
+    uint8_t twcr;       // TWI Control Register
+    uint8_t twamr;      // TWI (Slave) Address Mask Register
+} volatile twi_t;
+
+twi_t *twi = (twi_t *) (0xB8);
+
+/**********************************************************************
+ * RECEIVED DATA STORAGE (MR MODE)
+ **********************************************************************/
+
+uint8_t *twi_received_data;
+
+void free_storage() {
+    free(twi_received_data);
+    twi_received_data = NULL;
+}
+
+int init_data(const size_t n) {
+    free_storage();
+    twi_received_data = (uint8_t *)malloc(n * sizeof(uint8_t));
+    if (twi_received_data == NULL) {
+        return ERR_MEMORY_ALLOCATION_FAILED;
+    }
+    return SUCCESS;
+}
+
+uint8_t* get_received_data() {
+    return twi_received_data;
+}
+
+/**********************************************************************
+ * TWI FUNCTIONS
+ **********************************************************************/
+
+static uint8_t get_status(void) {
+    return twi->twsr & STATUS_REG_MASK;
+}
+
+void master_init(void) {
+    twi->twsr = PRESCALER_16;
+    twi->twbr = (F_CPU / F_I2C - 16) / (2 * 16);
+    twi->twcr = ENABLE_TWI;
+}
+
+void sla_slave_init(const uint8_t sla) {
+    twi->twar = sla << TWA0;
+    twi->twcr = ENABLE_TWI | ENABLE_ACK;
+}
+
+void slave_init() {
+    sla_slave_init(DEFAULT_SLA);
+}
+
+static uint8_t twi_start(void) {
+    twi->twcr = CLEAR_INT | GEN_START | ENABLE_TWI;
+    while (!I2C_TRANSMISSION_COMPLETE)
+        ;
+    return get_status();
+}
+
+static void twi_stop(void) {
+    twi->twcr = GEN_STOP | CLEAR_INT | ENABLE_TWI;
+}
+
+static uint8_t transmit(const uint8_t data) {
+    twi->twdr = data;
+    twi->twcr = CLEAR_INT | ENABLE_TWI;
+    while (!I2C_TRANSMISSION_COMPLETE)
+        ;
+    return get_status();
+}
+
+static void send_ACK() {
+    twi->twcr = CLEAR_INT | ENABLE_TWI | ENABLE_ACK;
+    while (!I2C_TRANSMISSION_COMPLETE)
+        ;
+}
+
+static void send_NACK() {
+    twi->twcr = CLEAR_INT | ENABLE_TWI;
+    while (!I2C_TRANSMISSION_COMPLETE)
+        ;
+}
+
+uint8_t twi_master_receive(const uint8_t tx_sla, const int n) {
+    if(init_data(n) == ERR_MEMORY_ALLOCATION_FAILED) {
+        return ERR_MEMORY_ALLOCATION_FAILED;
+    }
+    uint8_t status = 0;
+
+    status = twi_start();
+    if (status != START_CONDITION_TRANSMITTED) {
+        free_storage();
+        return ERR_START_FAILED;
+    }
+
+    status = transmit(SLA_R(tx_sla));
+    if (status == ARBITRATION_LOST) {
+        free_storage();
+        return ERR_ARBITRATION_LOST;
+    }
+
+    if (status == SLA_R_TRANSMITTED_NO_ACK_RECEIVED) {
+        free_storage();
+        twi_stop();
+        return ERR_NO_ACK;
+    }
+
+    for (int i = 0; i < n - 1; ++i) {
+        send_ACK();
+        twi_received_data[i] = twi->twdr;
+    }
+    send_NACK();
+    twi_received_data[n - 1] = twi->twdr;
+    twi_stop();
+    return SUCCESS;
+}
---
+++ drivers/TWI/twi.h
@@ -0,0 +1,134 @@
+/**********************************************************************
+ *
+ * twi.h: Interfaz del módulo TWI (I²C).
+ *
+ **********************************************************************/
+
+#ifndef _TWI_H
+#define _TWI_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// CONSTANTS
+#define F_CPU 16000000      // FRECUENCIA DEL PROCESADOR: 16 MHz
+#define F_I2C 100000        // FRECUENCIA DE COMUNICACIÓN: 100 kHz
+#define DEFAULT_SLA 0x10    // DIRECCIÓN DEL ESCLAVO POR DEFECTO
+
+// Return Status Code
+enum twi_status {
+ SUCCESS = 0,
+ ERR_START_FAILED = 1,
+ ERR_ARBITRATION_LOST = 2,
+ ERR_NO_ACK = 3,
+ ERR_MEMORY_ALLOCATION_FAILED = 4,
+};
+
+/**
+ * \brief Inicializa el TWI en modo maestro.
+ *
+ * Configura el registro de estado TWI con un prescaler de 16,
+ * establece el registro de bit rate TWI para la frecuencia de CPU y TWI,
+ * y habilita el TWI.
+ */
+void master_init(void);
+
+/**
+ * \brief Inicializa el TWI en modo esclavo.
+ *
+ * Configura el registro de dirección TWI con la dirección del esclavo
+ * y habilita el TWI y la generación de ACK.
+ */
+void slave_init(void);
+
+/**
+ * \brief Inicializa el TWI en modo esclavo con una dirección específica.
+ *
+ * Configura el registro de dirección TWI con la dirección del esclavo proporcionada
+ * y habilita el TWI y la generación de ACK.
+ *
+ * \param[in] sla Dirección del esclavo a configurar.
+ */
+void sla_slave_init(const uint8_t sla);
+
+/**
+ * \brief Genera una condición de inicio en el bus TWI.
+ *
+ * Configura el registro de control TWI para generar una condición de inicio
+ * y espera hasta que la transmisión se complete.
+ *
+ * \return Código de estado de la operación de inicio.
+ */
+uint8_t twi_start(void);
+
+/**
+ * \brief Genera una condición de parada en el bus TWI.
+ *
+ * Configura el registro de control TWI para generar una condición de parada.
+ */
+void twi_stop(void);
+
+/**
+ * \brief Transmite un byte de datos en el bus TWI.
+ *
+ * Configura el registro de datos TWI con el byte a transmitir y espera
+ * hasta que la transmisión se complete.
+ *
+ * \param[in] data Byte de datos a transmitir.
+ * \return Código de estado de la operación de transmisión.
+ */
+uint8_t transmit(const uint8_t data);
+
+/**
+ * \brief Envía una señal de reconocimiento (ACK) en el bus TWI.
+ *
+ * Configura el registro de control TWI para enviar una señal de reconocimiento
+ * y espera hasta que la transmisión se complete.
+ */
+void send_ACK(void);
+
+/**
+ * \brief Envía una señal de no reconocimiento (NACK) en el bus TWI.
+ *
+ * Configura el registro de control TWI para enviar una señal de no reconocimiento
+ * y espera hasta que la transmisión se complete.
+ */
+void send_NACK(void);
+
+/**
+ * \brief Recibe datos en modo maestro desde un esclavo TWI.
+ *
+ * Inicia una condición de inicio, transmite la dirección del esclavo con el bit de lectura,
+ * y recibe los datos del esclavo.
+ *
+ * \param[in] tx_sla Dirección del esclavo desde el cual recibir los datos.
+ * \param[in] n Número de bytes a recibir.
+ * \return Código de estado de la operación de recepción.
+ */
+uint8_t twi_master_receive(const uint8_t tx_sla, const int n);
+
+/**
+ * \brief Libera la memoria asignada para el almacenamiento de datos recibidos.
+ */
+void free_storage(void);
+
+/**
+ * \brief Inicializa el almacenamiento de datos recibidos.
+ *
+ * Asigna memoria para almacenar los datos recibidos.
+ *
+ * \param[in] n Número de bytes a almacenar.
+ * \return Código de estado de la operación de inicialización.
+ */
+int init_data(const size_t n);
+
+/**
+ * \brief Obtiene los datos recibidos.
+ *
+ * Devuelve un puntero a los datos recibidos almacenados.
+ *
+ * \return Puntero a los datos recibidos.
+ */
+uint8_t* get_received_data(void);
+
+#endif /* _TWI_H */
