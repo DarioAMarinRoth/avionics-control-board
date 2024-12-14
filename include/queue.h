cdf9e483e92f176d5ad4b1e6cd645eb31c88618f
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-12-14 14:13:15 -0300

    doc: se documenta el encabezado `queue.h`

--- include/queue.h
+++ include/queue.h
@@ -7,24 +7,73 @@
 
 #include <stdint.h>
 
+/**
+ * \brief Estructura que representa un nodo en la cola.
+ */
 typedef struct node {
-    uint8_t data;
-    struct node *next;
+    uint8_t data;       /**< Dato almacenado en el nodo. */
+    struct node *next;  /**< Puntero al siguiente nodo en la cola. */
 } node;
 
+/**
+ * \brief Estructura que representa una cola.
+ */
 typedef struct queue {
-    node *head;
-    node *tail;
+    node *head; /**< Puntero al primer nodo de la cola. */
+    node *tail; /**< Puntero al último nodo de la cola. */
 } queue;
 
+/**
+ * \brief Crea un nuevo nodo con el dato especificado.
+ * \param data Dato a almacenar en el nodo.
+ * \return Puntero al nodo creado.
+ */
 node *create_node(uint8_t data);
+
+/**
+ * \brief Destruye un nodo.
+ * \param n Puntero al nodo a destruir.
+ */
 void destroy_node(node *n);
 
+/**
+ * \brief Crea una nueva cola.
+ * \return Puntero a la cola creada.
+ */
 queue *create_queue(void);
+
+/**
+ * \brief Añade un dato a la cola.
+ * \param q Puntero a la cola.
+ * \param data Dato a añadir a la cola.
+ */
 void enqueue(queue *q, uint8_t data);
+
+/**
+ * \brief Obtiene el primer dato de la cola sin eliminarlo.
+ * \param q Puntero a la cola.
+ * \return El primer dato de la cola.
+ */
 int peek(queue *q);
+
+/**
+ * \brief Elimina y obtiene el primer dato de la cola.
+ * \param q Puntero a la cola.
+ * \return El primer dato de la cola.
+ */
 int dequeue(queue *q);
+
+/**
+ * \brief Verifica si la cola está vacía.
+ * \param q Puntero a la cola.
+ * \return 1 si la cola está vacía, 0 en caso contrario.
+ */
 int is_empty(queue *q);
+
+/**
+ * \brief Destruye una cola y libera la memoria asociada.
+ * \param q Puntero a la cola a destruir.
+ */
 void destroy_queue(queue *q);
 
 #endif //QUEUE_H
