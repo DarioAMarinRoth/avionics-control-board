65c2d3a5daef94fa49207a7519809a8bebc500be
Dario Marin Roth <dariomarinroth@gmail.com>
2025-05-08 13:10:25 -0300

    feat: se agrega un código de ejemplo con xinu utilizando dos tareas: - Comunicación I2C + serial - Alternar el led integrado

--- src/xinu-avr-pse/main/main.c
+++ src/xinu-avr-pse/main/main.c
@@ -2,24 +2,46 @@
 // Created by dario on 30/12/24.
 //
 
-#include "twi-slave.h"
+#include <stdint.h>
+#include <xinu.h>
+
+#include "twi-master.h"
 #include "matrix-keyboard.h"
+#include "serial.h"
+#include "gpio.h"
+
+void led() {
+    gpio_output(13);
+    while(1){
+        gpio_pin(13,ON);
+        sleepms(500);
+        gpio_pin(13,OFF);
+        sleepms(500);
+    }
+}
 
 void main() {
 
-    matrix_init();
-    twi_slave_init();
-    my_queue *data = get_toggle_events();
+    twi_master_init();
+    serial_init();
 
+    resume(create(led,128, 20, "led", 0));
+
+    uint8_t buttons[N_INPUTS];
+    for (int i = 0; i < N_INPUTS; i++) {
+        buttons[i] = 0;
+    }
+    const uint8_t change = 0;
     while (1) {
-        scan();
-        if (f_is_empty(data)) {
-            twi_slave_transmit(-1);
-            continue;
-        }
-        do {
-            twi_slave_transmit(f_pop(data));
-        } while (!f_is_empty(data));
+        // twi_master_receive_byte(DEFAULT_SLA);
+        // const uint8_t change = twi_get_received_data();
+        buttons[change] = !buttons[change];
 
+        for (int i = 0; i < N_INPUTS; i++) {
+            serial_put_int(buttons[i], 1);
+            serial_put_str(" ");
+        }
+        serial_put_str("\r");
     }
 }
+
