7f1645be153d05b4e7184f035d72dcbafc9b9ad7
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-06-26 11:31:06 -0300

    feat: se prueba la  recepción de la cola en simultaneo con otra tarea

--- testbench/src/tests/integration/communication/send-queue.c
+++ testbench/src/tests/integration/communication/send-queue.c
@@ -10,18 +10,26 @@
 
 void main() {
     my_queue *data = f_create_queue();
-    f_push(data,4);
-    f_push(data,0);
-    f_push(data,6);
-    f_push(data,1);
-    f_push(data,2);
-    f_push(data,5);
-    f_push(data,8);
-    f_push(data,9);
-
+    uint8_t msg[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    uint8_t size = 8;
     twi_slave_init();
-    twi_slave_transmit(f_get_size(data));
-    while (!f_is_empty(data)) {
-        twi_slave_transmit(f_pop(data));
+    while (1) {
+        for (int i = size - 1; i >= 0; --i) {
+            f_push(data, msg[i]);
+        }
+        twi_slave_transmit(f_get_size(data));
+        while (!f_is_empty(data)) {
+            twi_slave_transmit(f_pop(data));
+        }
+
+        msg[0]++;
+        for (int i = 0; i < size; ++i) {
+            if (msg[i] == 10) {
+                msg[i] = 0;
+                if (msg[i + 1] < size) {
+                    msg[i + 1]++;
+                }
+            }
+        }
     }
 }
--- xinu-avr-pse/main/main.c
+++ xinu-avr-pse/main/main.c
@@ -1,16 +1,42 @@
 //
-// Created by dario on 30/12/24.
+// Created by dario on 6/19/25.
 //
 
 #include <stdint.h>
 #include <xinu.h>
 
+#include "my_queue.h"
 #include "twi-master.h"
-#include "matrix-keyboard.h"
 #include "serial.h"
 #include "gpio.h"
 
-void led() {
+void receive_queue() {
+    my_queue *data = f_create_queue();
+
+    serial_init();
+    serial_put_str("Comenzando...\n\r");
+    twi_master_init();
+    while(1) {
+
+        twi_master_receive_byte(DEFAULT_SLA);
+        serial_put_str("El tamaño de la cola se recibió correctamente.\n\r");
+        const uint8_t data_size = twi_get_received_data();
+        for (int i = 0; i < data_size; ++i) {
+            twi_master_receive_byte(DEFAULT_SLA);
+            f_push(data, twi_get_received_data());
+        }
+        serial_put_str("Los datos recibidos son:\n\r");
+        for (int i = 0; i < data_size; ++i) {
+            serial_put_int(f_pop(data), 1);
+        }
+        serial_put_str("\n\r");
+        sleepms(500);
+    }
+}
+
+void main() {
+    resume(create(receive_queue, 256, 20, "rxq",0));
+
     gpio_output(13);
     while(1){
         gpio_pin(13,ON);
@@ -18,30 +44,6 @@ void led() {
         gpio_pin(13,OFF);
         sleepms(500);
     }
-}
 
-void main() {
-
-    twi_master_init();
-    serial_init();
-
-    resume(create(led,128, 20, "led", 0));
-
-    uint8_t buttons[N_INPUTS];
-    for (int i = 0; i < N_INPUTS; i++) {
-        buttons[i] = 0;
-    }
-    const uint8_t change = 0;
-    while (1) {
-        // twi_master_receive_byte(DEFAULT_SLA);
-        // const uint8_t change = twi_get_received_data();
-        buttons[change] = !buttons[change];
-
-        for (int i = 0; i < N_INPUTS; i++) {
-            serial_put_int(buttons[i], 1);
-            serial_put_str(" ");
-        }
-        serial_put_str("\r");
-    }
 }
 
