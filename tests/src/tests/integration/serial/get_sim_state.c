bbe2306304153c80664ddb21162df6611dcd6a80
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-08-21 10:52:47 -0300

    feat(serial): se agrega una función que recibe enteros sin signo de 16 bits

--- tests/src/tests/integration/serial/get_sim_state.c
+++ tests/src/tests/integration/serial/get_sim_state.c
@@ -9,8 +9,17 @@ void main() {
     int led = 13;
     gpio_output(led);
     serial_init();
+    serial_put_str("COMENZANDO... \n\r");
+
     while (1) {
-        if (serial_get_char() == '1') { // TODO: ver la forma de recibir un entero en lugar de un char
+        serial_put_str("Esperando mensaje \n\r");
+        uint16_t msg = serial_get_uint(5);
+        serial_put_str("Mensaje recibido:");
+        serial_put_int(msg, 5);
+        serial_put_str("\n\r");
+        serial_put_str("\n\r");
+        serial_put_str("\n\r");
+        if (msg == 12345) {
             gpio_pin(led, ON);
         } else {
             gpio_pin(led, OFF);
