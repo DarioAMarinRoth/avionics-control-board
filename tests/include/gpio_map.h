7da3d9b3c70f122e109cd956c890b0adb18c7d27
Dario A. Marin Roth <dariomarinroth@gmail.com>
2025-09-08 16:51:06 -0300

    refactor(gpio): se modifica el driver de la gpio para que sea más genérico. Ahora utiliza los headers de AVR en combinación con un segundo archivo que mapea cada etiqueta de la placa su bit/registro correspondiente. También se borran las funciones que no se utilizaban en el proyecto

---
+++ tests/include/gpio_map.h
@@ -0,0 +1,18 @@
+//
+// Created by dario on 8/9/25.
+//
+
+#ifndef GPIO_MAP_H
+#define GPIO_MAP_H
+
+#include <stdint.h>
+#include <avr/io.h>
+
+typedef struct {
+    volatile uint8_t *ddr;
+    volatile uint8_t *port;
+    volatile uint8_t *pin;
+    uint8_t bit;
+} gpio_t;
+
+#endif //GPIO_MAP_H
